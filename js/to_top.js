597f708cff22ee77c8c45760147bb0b3a5eb9e13
Rob Sullivan <robertosullivan5@gmail.com>
2020-12-19 15:55:20 +0000

    desktop updates

---
+++ js/to_top.js
@@ -0,0 +1,19 @@
+//Get the button
+var top_button = document.getElementById("top-button");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    top_button.style.display = "block";
+    } else {
+    top_button.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
